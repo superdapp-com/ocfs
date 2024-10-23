3bdf6336a05fa9e62e2e8f8a69893019f785f689
Joe Blau <im@joeblau.com>
2024-10-22 21:03:12 -0700

    feat: update sync file

--- sync.sh
+++ sync.sh
@@ -3,8 +3,8 @@
 # remove all .DS_Store files in every directory
 find . -name '.DS_Store' -type f -delete
 
-
+# build dist folder
 bun dist
 
 # clone prod directory to r2
-# rclone copy prod/ ofcs:ocfs
+rclone copy dist/ r2ocfs:ocfs
