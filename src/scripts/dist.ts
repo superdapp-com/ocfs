b8ab220dc9a1bd0ff557f6a8a3ccda86252016d1
Joe Blau <im@joeblau.com>
2024-10-23 07:36:32 -0700

    feat: add chain image

--- src/scripts/dist.ts
+++ src/scripts/dist.ts
@@ -11,26 +11,31 @@ export const dist = () => {
         const chainFolder = getChainFolder(chain);
         fs.mkdirSync(`${outBasePath}/${chain.id}`, { recursive: true });
 
-        const tokenListPath = `${inBasePath}/${chainFolder}/tokenList.json`;
-        const protocolListPath = `${inBasePath}/${chainFolder}/protocolList.json`;
-        fs.copyFileSync(tokenListPath, `${outBasePath}/${chain.id}/tokenList.json`);
-        fs.copyFileSync(protocolListPath, `${outBasePath}/${chain.id}/protocolList.json`);
+        const inChainBasePath = `${inBasePath}/${chainFolder}`;
+        const outChainBasePath = `${outBasePath}/${chain.id}`;
 
-        fs.mkdirSync(`${outBasePath}/${chain.id}/abi`, { recursive: true });
-        fs.mkdirSync(`${outBasePath}/${chain.id}/erc20`, { recursive: true });
+        const chainLogoPath = `${inChainBasePath}/image.svg`;
+        const tokenListPath = `${inChainBasePath}/tokenList.json`;
+        const protocolListPath = `${inChainBasePath}/protocolList.json`;
+        fs.copyFileSync(chainLogoPath, `${outBasePath}/${chain.id}.svg`);
+        fs.copyFileSync(tokenListPath, `${outChainBasePath}/tokenList.json`);
+        fs.copyFileSync(protocolListPath, `${outChainBasePath}/protocolList.json`);
+
+        fs.mkdirSync(`${outChainBasePath}/abi`, { recursive: true });
+        fs.mkdirSync(`${outChainBasePath}/erc20`, { recursive: true });
 
         const protocolList = JSON.parse(fs.readFileSync(protocolListPath, "utf8"));
         for (const protocol of protocolList.protocols) {
             const { name, address } = protocol;
-            fs.copyFileSync(`${sharedBasePath}/protocols/${name}/abi.json`, `${outBasePath}/${chain.id}/abi/${name}_${address}.json`);
+            fs.copyFileSync(`${sharedBasePath}/protocols/${name}/abi.json`, `${outChainBasePath}/abi/${name}_${address}.json`);
         }
 
         const tokenList = JSON.parse(fs.readFileSync(tokenListPath, "utf8"));
         for (const token of tokenList.tokens) {
             const tokenNameSymbol = getTokenNameSymbol(token);
             const tokenName = tokenNameSymbol.split("_")[0];
-            fs.copyFileSync(`${sharedBasePath}/images/${tokenName}/image.svg`, `${outBasePath}/${chain.id}/${token.address}.svg`);
-            fs.copyFileSync(`${inBasePath}/${chainFolder}/tokens/${tokenNameSymbol}/${token.address}/erc20.json`, `${outBasePath}/${chain.id}/erc20/${token.address}.json`);
+            fs.copyFileSync(`${sharedBasePath}/images/${tokenName}/image.svg`, `${outChainBasePath}/${token.address}.svg`);
+            fs.copyFileSync(`${inChainBasePath}/tokens/${tokenNameSymbol}/${token.address}/erc20.json`, `${outChainBasePath}/erc20/${token.address}.json`);
         }
     }
 
