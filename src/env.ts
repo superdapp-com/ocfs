93eb2208499243224d865cefd293f651fa16df79
Joe Blau <im@joeblau.com>
2024-12-08 17:16:16 -0800

    feat: add polygon

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     RPC_HTTP_1: z.string(),
+    RPC_HTTP_137: z.string(),
     RPC_HTTP_369: z.string(),
     RPC_HTTP_8453: z.string(),
   },
@@ -12,6 +13,7 @@ export const env = createEnv({
   },
   runtimeEnv: {
     RPC_HTTP_1: process.env.RPC_HTTP_1,
+    RPC_HTTP_137: process.env.RPC_HTTP_137,
     RPC_HTTP_369: process.env.RPC_HTTP_369,
     RPC_HTTP_8453: process.env.RPC_HTTP_8453,
   },
