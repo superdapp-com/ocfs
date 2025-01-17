aec89a76fcce8d10537847a1fcbea555c2146f15
Joe Blau <im@joeblau.com>
2025-01-16 21:59:22 -0800

    feat: add to deployment

--- src/scripts/svg-to-webp.sh
+++ src/scripts/svg-to-webp.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
 for file in $(find ./dist -type f -name "*.svg"); do
-    echo "Converting $file to ${file%.svg}.webp"
-    convert -quality 100 $file ${file%.svg}.webp
-    # convert -quality 100 $file ${file%.svg}.webp
+    convert -quality 100 -background none -resize 256x256 $file ${file%.svg}.webp
 done
