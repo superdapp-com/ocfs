6836c4a34fcc5c7e6632f8062b4cb31f406fd67f
Joe Blau <im@joeblau.com>
2025-01-16 22:44:45 -0800

    feat: install inkscape

--- src/scripts/svg-to-webp.sh
+++ src/scripts/svg-to-webp.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 for file in $(find ./dist -type f -name "*.svg"); do
-    convert -quality 100 -background none -resize 256x256 $file ${file%.svg}.webp
+    inkscape --export-type=png --export-width=256 --export-height=256 $file -o ${file%.svg}.webp && convert ${file%.svg}.png ${file%.svg}.webp && rm ${file%.svg}.png
 done
 
 # convert all webp to webp smaller 256 
 for file in $(find ./dist -type f -name "*.webp"); do
-    convert -quality 100 -background none -resize 256x256 $file ${file%.webp}.webp
+    inkscape --export-type=webp --export-width=256 --export-height=256 $file -o ${file%.webp}.webp  && convert ${file%.svg}.png ${file%.svg}.webp && rm ${file%.svg}.png
 done
