36c62306073e717f75b07152290a0e5812866a4f
Joe Blau <im@joeblau.com>
2025-01-16 22:51:03 -0800

    feat: fix script

--- src/scripts/svg-to-webp.sh
+++ src/scripts/svg-to-webp.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 for file in $(find ./dist -type f -name "*.svg"); do
-    inkscape --export-type=png --export-width=256 --export-height=256 $file -o ${file%.svg}.webp && convert ${file%.svg}.png ${file%.svg}.webp && rm ${file%.svg}.png
+    inkscape --export-type=png --export-width=256 --export-height=256 $file -o ${file%.svg}.png && convert ${file%.svg}.png ${file%.svg}.webp && rm ${file%.svg}.png
 done
 
 # convert all webp to webp smaller 256 
 for file in $(find ./dist -type f -name "*.webp"); do
-    inkscape --export-type=webp --export-width=256 --export-height=256 $file -o ${file%.webp}.webp  && convert ${file%.svg}.png ${file%.svg}.webp && rm ${file%.svg}.png
+    inkscape --export-type=png --export-width=256 --export-height=256 $file -o ${file%.webp}.png  && convert ${file%.svg}.png ${file%.svg}.webp && rm ${file%.svg}.png
 done
