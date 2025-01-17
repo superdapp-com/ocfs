4489ae8b5ab59f4bcc13270811dfa30286daf0b2
Joe Blau <im@joeblau.com>
2025-01-16 22:08:57 -0800

    feat: update webps

--- src/scripts/svg-to-webp.sh
+++ src/scripts/svg-to-webp.sh
@@ -3,3 +3,8 @@
 for file in $(find ./dist -type f -name "*.svg"); do
     convert -quality 100 -background none -resize 256x256 $file ${file%.svg}.webp
 done
+
+# convert all webp to webp smaller 256 
+for file in $(find ./dist -type f -name "*.webp"); do
+    convert -quality 100 -background none -resize 256x256 $file ${file%.webp}.webp
+done
