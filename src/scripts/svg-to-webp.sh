05243f2fb9d8aa6305d6e57dc8f11cc910aadff5
Joe Blau <im@joeblau.com>
2025-02-22 19:53:49 -0800

    feat: update more tokens

--- src/scripts/svg-to-webp.sh
+++ src/scripts/svg-to-webp.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 # convert all webp to webp smaller 256
-for file in $(find ./dist -type f -name "*.webp"); do
-    convert -quality 100 -background none -resize 256x256 $file ${file%.webp}.webp
-done
+# for file in $(find ./dist -type f -name "*.webp"); do
+#     magick -quality 100 -background none -resize 256x256 $file ${file%.webp}.webp
+# done
 
 for file in $(find ./dist -type f -name "*.svg"); do
     inkscape --export-type=png --export-width=256 --export-height=256 $file -o ${file%.svg}.png
-    convert ${file%.svg}.png ${file%.svg}.webp
+    magick ${file%.svg}.png ${file%.svg}.webp
     rm ${file%.svg}.png
 done
 
