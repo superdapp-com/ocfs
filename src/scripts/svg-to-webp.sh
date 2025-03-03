ce84534ed27c339b11382569207570d2a6d03d48
Joe Blau <im@joeblau.com>
2025-03-02 20:31:08 -0800

    feat: add gtk

--- src/scripts/svg-to-webp.sh
+++ src/scripts/svg-to-webp.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
-# convert all webp to webp smaller 256
-# for file in $(find ./dist -type f -name "*.webp"); do
-#     convert -quality 100 -background none -resize 256x256 $file ${file%.webp}.webp
-# done
+# Initialize GTK before using Inkscape to prevent GTK warnings
+export XDG_RUNTIME_DIR="/tmp/runtime-root"
+mkdir -p $XDG_RUNTIME_DIR
 
 for file in $(find ./dist -type f -name "*.svg"); do
-    inkscape --batch-process --export-type=png --export-width=256 --export-height=256 $file -o ${file%.svg}.png
+    inkscape --without-gui --batch-process --export-type=png --export-width=256 --export-height=256 $file -o ${file%.svg}.png
     convert ${file%.svg}.png ${file%.svg}.webp
     rm ${file%.svg}.png
 done
-
