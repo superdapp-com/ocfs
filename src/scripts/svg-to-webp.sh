fc3c3afa28fc00c5abadb5c04c44b1c0d465cd0e
Joe Blau <im@joeblau.com>
2025-03-02 20:17:57 -0800

    feat: fix inkscape warnings

--- src/scripts/svg-to-webp.sh
+++ src/scripts/svg-to-webp.sh
@@ -6,8 +6,8 @@
 # done
 
 for file in $(find ./dist -type f -name "*.svg"); do
-    inkscape --export-type=png --export-width=256 --export-height=256 $file -o ${file%.svg}.png
+    # Add --export-option=suppressCSSWarnings to ignore CSS warnings like mask-type
+    inkscape --batch-process --export-type=png --export-width=256 --export-height=256 --export-option=suppressCSSWarnings $file -o ${file%.svg}.png
     convert ${file%.svg}.png ${file%.svg}.webp
     rm ${file%.svg}.png
 done
-
