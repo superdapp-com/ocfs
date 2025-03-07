f1d2448c27e3483bf6414ab8ce79b773b2f8985f
Joe Blau <im@joeblau.com>
2025-03-06 19:29:13 -0800

    feat: update rpcs

--- src/scripts/svg-to-webp.sh
+++ src/scripts/svg-to-webp.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
-# Initialize GTK before using Inkscape to prevent GTK warnings
-export XDG_RUNTIME_DIR="/tmp/runtime-root"
-mkdir -p $XDG_RUNTIME_DIR
 
 for file in $(find ./dist -type f -name "*.svg"); do
-    inkscape --without-gui --batch-process --export-type=png --export-width=256 --export-height=256 $file -o ${file%.svg}.png
+    inkscape --export-type=png --export-width=256 --export-height=256 $file -o ${file%.svg}.png
     convert ${file%.svg}.png ${file%.svg}.webp
     rm ${file%.svg}.png
 done
+
